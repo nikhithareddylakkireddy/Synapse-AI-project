4334d8c5be5f5e5f735f79eeb7b7bea26b489320
nikhithareddylakkireddy <nikhithareddylakkireddy@gmail.com>
2026-03-08 22:43:34 +0530

    safe push without api key

--- ai_utils.py
+++ ai_utils.py
@@ -1,17 +1,24 @@
+from dotenv import load_dotenv
+import os
 from openai import OpenAI
 
+load_dotenv()
+
 client = OpenAI(
-    api_key="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",
-    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
 )
 
 def ask_ai(prompt):
 
     completion = client.chat.completions.create(
-        model="openai/gpt-3.5-turbo",
+        model="meta-llama/llama-3-8b-instruct",
         messages=[
             {"role": "user", "content": prompt}
         ]
     )
 
     return completion.choices[0].message.content
+
+
+print(ask_ai("Explain artificial intelligence in simple words"))
